1a29f74c8e7d51b0b740195839a7d4736cd3ed1d
Suplanus <info@suplanus.de>
2019-03-26 11:04:31 +0100

    TIA

--- src/Siemens/ApiShowcase.Siemens.Openness.Demo/Program.cs
+++ src/Siemens/ApiShowcase.Siemens.Openness.Demo/Program.cs
@@ -2,12 +2,17 @@
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using Microsoft.Win32;
 using Siemens.Engineering;
+using Siemens.Engineering.Compiler;
 using Siemens.Engineering.Hmi;
-using Siemens.Engineering.HmiUnified;
-using Siemens.Engineering.HW;
 using Siemens.Engineering.HW.Features;
 using Siemens.Engineering.SW;
+using Siemens.Engineering.SW.Blocks;
+using Siemens.Engineering.SW.ExternalSources;
 
 namespace ApiShowcase.Siemens.Openness.Demo
 {
@@ -19,9 +24,44 @@ namespace ApiShowcase.Siemens.Openness.Demo
     private static PlcSoftware _plcSoftware;
     private static HmiTarget _hmiTarget;
 
-    static void Main(string[] args)
+    static void Main()
     {
-      // Start or connect to TIA Portal
+      Stopwatch sw = new Stopwatch();
+      sw.Restart();
+
+      InitTiaPortal();
+
+      try
+      {
+        var project = CreateProject();
+        CreateDevices(project);
+        LoadSclSources(_plcSoftware);
+        InsertHmiPages(_hmiTarget);
+        Compile();
+        ShowBlockInEditor();
+      }
+      finally
+      {
+        TiaPortalProcess tiaProcess = _tiaPortal.GetCurrentProcess();
+        var process = Process.GetProcessById(tiaProcess.Id);
+        _tiaPortal.Dispose();
+        Console.WriteLine($"Finished in {sw.Elapsed.Seconds}s");
+        WindowHelper.BringProcessToFront(process);
+
+        //Console.WriteLine();
+        //Console.WriteLine("Press any key to continue...");
+        //Console.ReadKey();
+      }
+    }
+
+    private static void InitTiaPortal()
+    {
+      Console.WriteLine("Check firewall...");
+
+      SetTiaPortalFirewall();
+
+      // Open TIA
+      Console.WriteLine("Init TIA Portal...");
       var tiaPortalProcesses = TiaPortal.GetProcesses();
       if (tiaPortalProcesses.Any())
       {
@@ -31,87 +71,52 @@ namespace ApiShowcase.Siemens.Openness.Demo
       {
         _tiaPortal = new TiaPortal(TiaPortalMode.WithUserInterface);
       }
-
-      try
-      {
-        var project = CreateProject();
-        CreateDevices(project);
-
-        LoadSclSources(_plcSoftware);
-
-        InsertHmiPages(_hmiTarget);
-      }
-      finally
-      {
-        _tiaPortal.Dispose();
-      }
     }
 
-    private static void LoadSclSources(PlcSoftware plcSoftware)
+    public static void SetTiaPortalFirewall()
     {
-      // todo
-    }
-
-    private static void InsertHmiPages(HmiTarget hmiTarget)
-    {
-      // todo
-    }
-
-    private static void CreateDevices(Project project)
-    {
-      // Get existing
-      GetDeviceSoftware(project);
-
-      // Create
-      if (_plcSoftware == null || _hmiTarget == null)
+      // Check if admin
+      WindowsIdentity identity = WindowsIdentity.GetCurrent();
+      WindowsPrincipal principal = new WindowsPrincipal(identity);
+      bool isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+      if (!isAdmin)
       {
-        if (_plcSoftware == null)
-        {
-          project.Devices.CreateWithItem("OrderNumber:6ES7 510-1DJ01-0AB0/V2.0", "PLC_1", "MyPlc");  
-        }
-        if (_hmiTarget == null)
-        {
-          // todo
-          project.Devices.CreateWithItem("OrderNumber:6AV2 123-2GA03-0AX0/15.1.0.0", "HMI_1", "MyHmi");  
-        }
-
-        GetDeviceSoftware(project);
+        return;
       }
-    }
 
-    private static void GetDeviceSoftware(Project project)
-    {
-      foreach (var device in project.Devices)
+      Assembly assembly = Assembly.GetExecutingAssembly();
+      string exePath = assembly.Location;
+
+      // Get hash
+      HashAlgorithm hashAlgorithm = SHA256.Create();
+      FileStream stream = File.OpenRead(exePath);
+      byte[] hash = hashAlgorithm.ComputeHash(stream);
+      string convertedHash = Convert.ToBase64String(hash);
+
+      // Get date
+      FileInfo fileInfo = new FileInfo(exePath);
+      DateTime lastWriteTimeUtc = fileInfo.LastWriteTimeUtc;
+      string lastWriteTimeUtcFormatted = lastWriteTimeUtc.ToString("yyyy'/'MM'/'dd HH:mm:ss.fff");
+
+      // Get execution version
+      AssemblyName siemensAssembly = Assembly.GetExecutingAssembly().GetReferencedAssemblies().First(obj => obj.Name.Equals("Siemens.Engineering"));
+      string version = siemensAssembly.Version.ToString(2);
+
+      // Set key and values
+      string keyFullName = $@"SOFTWARE\Siemens\Automation\Openness\{version}\Whitelist\{fileInfo.Name}\Entry";
+      RegistryKey key = Registry.LocalMachine.CreateSubKey(keyFullName);
+      if (key == null)
       {
-        foreach (var deviceItem in device.DeviceItems)
-        {
-          // Get software
-          SoftwareContainer softwareContainer = ((IEngineeringServiceProvider)deviceItem).GetService<SoftwareContainer>();
-          if (softwareContainer != null)
-          {
-            switch (softwareContainer.Software)
-            {
-              case HmiTarget hmiTarget:
-                _hmiTarget = hmiTarget;
-                break;
-              case PlcSoftware plcSoftware:
-                _plcSoftware = plcSoftware;
-                break;
-            }
-          }
-
-          // All found
-          if (_hmiTarget != null && _plcSoftware != null)
-          {
-            return;
-          }
-        }
+        throw new Exception("Key note found: " + keyFullName);
       }
+      key.SetValue("Path", exePath);
+      key.SetValue("DateModified", lastWriteTimeUtcFormatted);
+      key.SetValue("FileHash", convertedHash);
     }
 
     private static Project CreateProject()
     {
-      Console.WriteLine("Create project: " + PROJECT_PATH);
+      Console.WriteLine("Create project...");
       Project project;
 
       // Open
@@ -135,6 +140,113 @@ namespace ApiShowcase.Siemens.Openness.Demo
         project = _tiaPortal.Projects.Create(directoryInfo, projectName);
       }
       return project;
+    }
+
+    private static void CreateDevices(Project project)
+    {
+      Console.WriteLine("Get devices...");
+
+      // Get existing
+      GetDeviceSoftware(project);
+
+      // Create
+      if (_plcSoftware == null || _hmiTarget == null)
+      {
+        if (_plcSoftware == null)
+        {
+          Console.WriteLine("Create PLC...");
+          project.Devices.CreateWithItem("OrderNumber:6ES7 211-1AD30-0XB0/V2.2", "PLC_1", null);
+        }
+        if (_hmiTarget == null)
+        {
+          Console.WriteLine("Create HMI...");
+          project.Devices.CreateWithItem("OrderNumber:6AV2 123-2GB03-0AX0/15.1.0.0", "HMI_1", null); // Name not allowed
+        }
+
+        GetDeviceSoftware(project);
+      }
+    }
+
+    private static void GetDeviceSoftware(Project project)
+    {
+      foreach (var device in project.Devices)
+      {
+        foreach (var deviceItem in device.DeviceItems)
+        {
+          // Get software
+          SoftwareContainer softwareContainer = ((IEngineeringServiceProvider)deviceItem).GetService<SoftwareContainer>();
+          if (softwareContainer != null)
+          {
+            switch (softwareContainer.Software)
+            {
+              case HmiTarget hmiTarget:
+                Console.WriteLine("Get HmiTarget...");
+                _hmiTarget = hmiTarget;
+                break;
+              case PlcSoftware plcSoftware:
+                Console.WriteLine("Get PlcSoftware...");
+                _plcSoftware = plcSoftware;
+                break;
+            }
+          }
+
+          // All found
+          if (_hmiTarget != null && _plcSoftware != null)
+          {
+            return;
+          }
+        }
+      }
+    }
+
+    private static void LoadSclSources(PlcSoftware plcSoftware)
+    {
+      // Create source
+      var sourceName = "CheckArray";
+      var source = plcSoftware.ExternalSourceGroup.ExternalSources.FirstOrDefault(obj => obj.Name.Equals(sourceName));
+      if (source == null)
+      {
+        string sourcePath = @"\\Mac\Home\Documents\GitHub\ibKastl.ApiShowcase\data\Siemens\CheckArray.scl";
+        source = plcSoftware.ExternalSourceGroup.ExternalSources.CreateFromFile(sourceName, sourcePath);
+      }
+
+      // Create block from source
+      PlcBlock block = plcSoftware.BlockGroup.Blocks.FirstOrDefault(obj => obj.Name.Equals(sourceName));
+      if (block == null)
+      {
+        GenerateBlockOption options = GenerateBlockOption.KeepOnError;
+        source.GenerateBlocksFromSource(options);
+      }
+    }
+
+    private static void InsertHmiPages(HmiTarget hmiTarget)
+    {
+      string screenFile = @"\\Mac\Home\Documents\GitHub\ibKastl.ApiShowcase\data\Siemens\Screen.xml";
+      FileInfo fileInfo = new FileInfo(screenFile);
+
+      // Export first time
+      if (!fileInfo.Exists)
+      {
+        hmiTarget.ScreenFolder.Screens.Last().Export(fileInfo, ExportOptions.WithDefaults);
+      }
+
+      hmiTarget.ScreenFolder.Screens.Import(fileInfo, ImportOptions.Override);
+    }
+
+    private static void Compile()
+    {
+      Console.WriteLine("Compile...");
+      _hmiTarget.GetService<ICompilable>().Compile();
+      _plcSoftware.GetService<ICompilable>().Compile();
+    }
+
+    private static void ShowBlockInEditor()
+    {
+      Console.WriteLine("Show block in editor...");
+      var block = _plcSoftware.BlockGroup.Blocks
+                              .OfType<OB>()
+                              .First(obj => obj.Number.Equals(1));
+      block.ShowInEditor();
     }
   }
 }
---
+++ src/Siemens/ApiShowcase.Siemens.Openness.Demo/WindowHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ApiShowcase.Siemens.Openness.Demo
+{
+  public static class WindowHelper
+  {
+    const int SW_RESTORE = 9;
+
+    public static void BringProcessToFront(Process process)
+    {
+      IntPtr handle = process.MainWindowHandle;
+      if (IsIconic(handle))
+      {
+        ShowWindow(handle, SW_RESTORE);
+      }
+
+      SetForegroundWindow(handle);
+    }
+
+    [DllImport("User32.dll")]
+    private static extern bool SetForegroundWindow(IntPtr handle);
+
+    [DllImport("User32.dll")]
+    private static extern bool ShowWindow(IntPtr handle, int nCmdShow);
+
+    [DllImport("User32.dll")]
+    private static extern bool IsIconic(IntPtr handle);
+  }
+}
